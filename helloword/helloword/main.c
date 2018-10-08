96ed2646891e03cb3e6ef8385184da23e496a9df
jyz <jyz@jyzdeimac.local>
2018-10-08 18:35:05 +0800

    123

---
+++ helloword/helloword/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  helloword
+//
+//  Created by jyz on 2018/10/8.
+//  Copyright © 2018年 jyz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
